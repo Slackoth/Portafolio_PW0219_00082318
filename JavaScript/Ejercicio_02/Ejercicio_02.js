91479b537fba92531f820265a2f79c3407ec24a1
Slackoth <00082318@uca.edu.sv>
2019-09-25 09:01:10 -0600

    Ejercicios de JS

---
+++ JavaScript/Ejercicio_02/Ejercicio_02.js
@@ -0,0 +1,106 @@
+/*Traspaso de datos a traves de JSon */
+let object = {
+    "squirrel": false,
+    "events": ["work", "touched tree", "pizza", "running"]
+  }
+  
+  let string = JSON.stringify({squirrel: false,
+                               events: ["weekend"]});
+  console.log(string);
+  console.log(JSON.parse(string));
+/*Abstracting*/
+function repeat(n, action) {
+  for (let i = 0; i < n; i++) {
+    action(i);
+  }
+}
+//1.
+let departments = [];
+repeat(5, i => departments.push(`Department ${i}`));
+console.log({departments}); // Only for get a name of the var
+//2.
+/*let par = [];
+repeat(1001, n => {
+    if (n % 2 === 0) {
+        par.push(n);
+    }
+});
+console.log(par);*/
+/*High-order functions: functions that operate on other functions*/
+//1.
+function lessThan(n) {
+    return m => m < n;
+}
+let lessThan10 = lessThan(10);
+console.log(lessThan10(4));
+//2. 
+function unless(test, then) {
+    if (!test) then();
+}
+  
+unless( 2 > 3, n => console.log("ok"))
+/*Array higher-order functions*/
+//['A', 'B'].forEach(i => console.log(i));
+
+//Ejercicio_01
+let arrayOne = [9,6,5,4,3,2,1,0];
+let even = [];
+//arrayOne.forEach(evenNumber);
+/*function evenNumber(num, index, array) { 
+    if(num % 2 === 0) {
+        even.push(num);
+        console.log(even);
+    }
+}*/
+function filter(array, test) {
+    let passed = [];
+    for (let element of array) {
+      if (test(element)) {
+        passed.push(element);
+      }
+    }
+    return passed;
+}
+filter([1,3,5,6,7,10], e => e % 2 == 0);
+//Ejercicio_02
+let journal = [
+    {
+        events: ['cut','sleep'],
+        accident: true
+    },
+    {
+        events: ['dance', 'cook'],
+        accident: false
+    },
+    {
+        events: ['cut', 'cook'],
+        accident: true
+    }
+];
+function Transform(array, test) {
+    let arrayTran = [];
+    for(let element of array) {
+
+    }
+}
+
+Transform(journal, );
+//Ejercicio_03
+/*[1,2,3,4,5]
+[3,2,4,5]
+[5,4,5]... */
+let arrayNum = [1,2,3,4,5];
+function Reduce(array) {
+    let arrayReduce = [];
+    for(let element of array) {
+        
+    }
+}
+let sum = arrayNum.indexOf(0) + arrayNum.indexOf(1);
+//console.log(sum);
+/*
+Reduce(arrayNum, array => {
+    return
+}); */
+
+
